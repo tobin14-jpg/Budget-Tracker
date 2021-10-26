465c317203a5445ee4e8fe842432e59f4fbf1318
Ice Tea <shauntobin88@gmail.com>
2021-10-26 14:43:25 +0800

    fixed deploying issue

--- server.js
+++ server.js
@@ -16,14 +16,13 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/budgetDB',
+  process.env.MONGODB_URI || "mongodb://localhost/budgetDB", 
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useFindAndModify: false,
     useCreateIndex: true,
     useFindAndModify: false
-  }
-);
+  });
 
 // routes
 app.use(require("./routes/api.js"));
