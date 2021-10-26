11044f6741a27db92ae4418392286a5a504958c9
Ice Tea <shauntobin88@gmail.com>
2021-10-26 15:03:16 +0800

    deploying

--- server.js
+++ server.js
@@ -15,14 +15,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/budgetDB", 
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    useFindAndModify: false
-  });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/WOTracker", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology:true,
+});
 
 // routes
 app.use(require("./routes/api.js"));
